48005addbd5a8bb44eadd46755236f2eab47592e
blueknarr <knarrblue@gmail.com>
2023-06-27 11:00:38 +0900

    firebase에 이미지 업로드

--- lib/create/create_model.dart
+++ lib/create/create_model.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../domain/post.dart';
@@ -20,6 +21,16 @@ class CreateModel {
   }
 
   Future<void> uploadPost(String title, File imageFile) async {
+    /// 이미지 업로드
+    final storageRef = FirebaseStorage.instance.ref();
+    final imageRef = storageRef
+        .child('postImages/${DateTime.now().microsecondsSinceEpoch}.jpg');
+    await imageRef.putFile(imageFile);
+
+    /// 이미지 url을 얻는다.
+    final downloadUrl = await imageRef.getDownloadURL();
+
+    /// 게시물 업로드
     final postsRef =
         FirebaseFirestore.instance.collection('posts').withConverter<Post>(
               fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
@@ -31,11 +42,11 @@ class CreateModel {
 
     newPostRef.set(
       Post(
-          id: newPostRef.id,
-          userId: FirebaseAuth.instance.currentUser?.uid ?? '',
-          title: title,
-          imageUrl:
-              'https://file2.nocutnews.co.kr/newsroom/image/2023/05/03/202305031714287586_0.jpg'),
+        id: newPostRef.id,
+        userId: FirebaseAuth.instance.currentUser?.uid ?? '',
+        title: title,
+        imageUrl: downloadUrl,
+      ),
     );
   }
 }
